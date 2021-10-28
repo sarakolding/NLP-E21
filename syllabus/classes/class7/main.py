2fab353bd96fd084780ff25dfb2e4b808cfe59d1
sarakolding <sarakolding@live.dk>
2021-10-28 15:20:20 +0200

    class 7 update

--- syllabus/classes/class7/main.py
+++ syllabus/classes/class7/main.py
@@ -1,21 +1,21 @@
-from typing import List
+import typing
 from datasets import load_dataset
 import gensim.downloader as api
 
-
 # DATASET
+# loading the conllpp dataset from the dataset package
 dataset = load_dataset("conllpp")
+# subsetting only the training data 
 train = dataset["train"]
 
 # inspect the dataset
-train["tokens"][:1]
-train["ner_tags"][:1]
-num_classes = train.features["ner_tags"].feature.num_classes
-
+print(train["tokens"][:1])
+print(train["ner_tags"][:1])
+# 9 classes
+num_classes = train.features["ner_tags"].feature.num_classes 
 
 # CONVERTING EMBEDDINGS
 import numpy as np
-
 import torch
 
 model = api.load("glove-wiki-gigaword-50")
@@ -23,11 +23,10 @@ model = api.load("glove-wiki-gigaword-50")
 from embedding import gensim_to_torch_embedding
 
 # convert gensim word embedding to torch word embedding
+# we want a torch rather than a numpy embedding
 embedding_layer, vocab = gensim_to_torch_embedding(model)
 
-
 # PREPARING A BATCH
-
 
 def tokens_to_idx(tokens, vocab=model.key_to_index):
     """
@@ -36,25 +35,29 @@ def tokens_to_idx(tokens, vocab=model.key_to_index):
     - What does the .get method do?
     - Why lowercase?
     """
+    # get function returns a value for a key in dicts 
+    # i.e. return index for word, if the word is unknown then return index for unknown
     return [vocab.get(t.lower(), vocab["UNK"]) for t in tokens]
 
 
-# sample batch of 10 sentences
+# subsetting sample batch of 10 sentences
 batch_tokens = train["tokens"][:10]
 batch_tags = train["ner_tags"][:10]
 batch_tok_idx = [tokens_to_idx(sent) for sent in batch_tokens]
 batch_size = len(batch_tokens)
 
-# compute length of longest sentence in batch
+# compute length of longest sentence in batch (to estimate padding)
 batch_max_len = max([len(s) for s in batch_tok_idx])
 
 # prepare a numpy array with the data, initializing the data with 'PAD'
 # and all labels with -1; initializing labels to -1 differentiates tokens
 # with tags from 'PAD' tokens
+# essentially creating a "skeleton" for the data with only padding (size max(length))
 batch_input = vocab["PAD"] * np.ones((batch_size, batch_max_len))
-batch_labels = -1 * np.ones((batch_size, batch_max_len))
+batch_labels = -1 * np.ones((batch_size, batch_max_len)) # the label for our padding is -1
 
 # copy the data to the numpy array
+# putting in the data with padding at the ends (pad = 0.000001?)
 for i in range(batch_size):
     tok_idx = batch_tok_idx[i]
     tags = batch_tags[i]
@@ -64,7 +67,7 @@ for i in range(batch_size):
     batch_labels[i][:size] = tags
 
 
-# since all data are indices, we convert them to torch LongTensors
+# since all data are indices, we convert them to torch LongTensors (integers)
 batch_input, batch_labels = torch.LongTensor(batch_input), torch.LongTensor(
     batch_labels
 )
@@ -73,12 +76,13 @@ batch_input, batch_labels = torch.LongTensor(batch_input), torch.LongTensor(
 from LSTM import RNN
 
 model = RNN(
-    embedding_layer=embedding_layer, num_classes=num_classes + 1, hidden_dim_size=256
+    embedding_layer=embedding_layer, output_dim=num_classes + 1, hidden_dim_size=256
 )
 
 # FORWARD PASS
 X = batch_input
 y = model(X)
 
+# this loss function removes everything with index of -1 (padding), otherwise as cross entropy
 loss = model.loss_fn(outputs=y, labels=batch_labels)
 # loss.backward()
